80d9fb0ced89e9772d89b2b3049098e7a2a4583f
JoeChapman <joseph.philip.chapman@gmail.com>
2018-01-03 16:33:02 +0000

    Amend Docker configuration

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 'use strict';
 
-var express = require('express'),
-  routes = require('./routes'),
-  router = express.Router(),
-  bodyParser = require('body-parser'),
-  cookieParser = require('cookie-parser'),
-  session = require('express-session'),
-  methodOverride = require('method-override'),
-  compression = require('compression'),
-  path = require('path'),
-  config = require('./config'),
-  stylus = require('stylus'),
-  nib = require('nib'),
-  app = express(),
-  server;
+const express = require('express');
+const routes = require('./routes');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const compression = require('compression');
+const path = require('path');
+const config = require('./config');
+const stylus = require('stylus');
+const nib = require('nib');
+const app = express();
+
+const morgan = require('morgan');
+app.use(morgan('combined'))
 
 function compile(str, path) {
   return stylus(str)
@@ -22,31 +24,32 @@ function compile(str, path) {
     .use(nib());
 }
 
-app
-  .disable('x-powered-by')
-  .locals.basedir = path.join(__dirname, 'views/');
+app.disable('x-powered-by')
+app.locals.basedir = path.join(__dirname, 'views/');
 
-app
-  .set('port', config.port)
-  .set('views', app.locals.basedir)
-  .set('view engine', 'jade');
+app.set('port', config.port)
+app.set('views', app.locals.basedir)
+app.set('view engine', 'jade');
 
-app
-  .use(compression())
-  .use(stylus.middleware({
-    src: path.join(__dirname, '/src'),
-    dest: path.join(__dirname, '/dist'),
-    compile: compile
-  }))
-  .use(express.static(path.join(__dirname, '/dist')))
-  .use(cookieParser())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(bodyParser.json())
-  .use(methodOverride(('X-HTTP-Method-Override')))
-  .use(session({ secret: config.secret, saveUninitialized: true, resave: true} ))
-  .use(routes(router));
+app.use(compression())
+app.use(stylus.middleware({
+  src: path.join(__dirname, '/src'),
+  dest: path.join(__dirname, '/dist'),
+  compile: compile
+}));
+app.use(express.static(path.join(__dirname, '/dist')))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(methodOverride(('X-HTTP-Method-Override')))
+app.use(session({
+  secret: config.secret,
+  saveUninitialized: true,
+  resave: true
+}));
+app.use(routes(router));
 
-server = app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function() {
   console.log('Seymour Admin server listening at %s', server.address().address, server.address().port);
 });
 
